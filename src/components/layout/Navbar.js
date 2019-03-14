591a264923687ecf39ea8b17ad74a258db833a3a
fisher <44139455+Mous-Fisher@users.noreply.github.com>
2019-03-14 15:59:48 -0500

    commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = ()=> {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 mx-auto">Find Lyrics To Any Song</span>
+    </nav>
+  )
+}
+
+
+
+export default Navbar ;
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () =>{
+
+    return(
+        <div>
+            <img src='https://acegif.com/wp-content/uploads/loading-4.gif'
+            alt="Loading..."
+             style={{width: '500px', margin: '40px auto', display: 'block'}}
+            />
+        </div>
+    )
+}
